48ea7520601779372519bfc829a70b92ac04e6b2
dosora1 <dosora9@gmail.com>
2022-10-13 01:00:34 +0900

    뷰페이저 삽입
    - 리사이클러뷰의 어댑터를 그대로 이용
    - datas 리스트에 들어가는 것은 TextView에 들어갈 문구(String)

--- app/src/main/java/com/example/kt_viewpager/MainActivity.kt
+++ app/src/main/java/com/example/kt_viewpager/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kt_viewpager
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.kt_viewpager.databinding.ActivityMainBinding
+import com.example.kt_viewpager.databinding.ItemPagerBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val datas = mutableListOf<String>()
+        for (i in 1..10) {
+            datas.add("item $i")
+        }
+        binding.viewpager.adapter = MyPagerAdapter(datas)
     }
 }
